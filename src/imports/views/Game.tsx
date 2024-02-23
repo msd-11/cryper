47c21f36c9dbbd2033c01ca19092770be3825ecc
msd-11 <msd0xB@pm.me>
2024-02-23 14:07:08 +0100

    add: carrousel component

--- src/imports/views/Game.tsx
+++ src/imports/views/Game.tsx
@@ -1,36 +1,99 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { useState, useEffect } from "react";
 
-function Game() {
+const data = [
+  { name: "Bitcoin", value: 1255.0 },
+  { name: "Etherum", value: 121 },
+  { name: "Nano", value: 255 },
+];
+
+const Game: React.FC = () => {
+  const [api, setApi] = useState<CarouselApi>();
+
+  const [current, setCurrent] = useState(0);
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      console.log("log");
+      setCount(count + 1);
+    });
+  }, [api]);
+
+  const handleClick = () => {
+    console.log("ok");
+    api?.scrollNext();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-28 h-28 bg-[#181A1B] z-10 rounded-[50%] flex justify-center items-center">
+        <p className="text-[3vh] font-[RooneySansRegular]">VS</p>
       </div>
-      <h1>Vite + React</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the TestazjekazjkazaaeVite and React logos to learn more
-      </p>
+      <Carousel setApi={setApi} opts={{ watchDrag: false, slidesToScroll: 2 }}>
+        <CarouselContent>
+          <CarouselItem className="relative basis-1/2 bg-white m-0">
+            <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-fit h-fit z-10 text-center">
+              <p className="text-[6vh] font-[RooneySansRegular]">“Greece”</p>
+              <p className="text-xl font-[RooneySansRegular]">has</p>
+              <p
+                onClick={handleClick}
+                className="text-[6vh] text-[#FFF879] font-[RooneySansRegular]"
+              >
+                673,000
+              </p>
+              <p className="text-xl font-[RooneySansRegular]">value</p>
+            </div>
+            <div className="h-screen w-[150%] text-white bg-[url('https://assets.coingecko.com/coins/images/1/large/bitcoin.png')] brightness-75 bg-no-repeat bg-cover bg-center blur-xl"></div>
+          </CarouselItem>
+
+          <CarouselItem className="relative basis-1/2 bg-white m-0">
+            <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-fit h-fit z-10 text-center">
+              <p className="text-[6vh] font-[RooneySansRegular]">“Greece”</p>
+              <p className="text-xl font-[RooneySansRegular]">has</p>
+              <div className="flex flex-col gap-4 mt-4">
+                <button className="border p-3 rounded-[2rem] text-[#FFF879] hover:bg-[#181A1B] hover:text-white">
+                  <p className="text-extrabold text-xl font-[RooneySansRegular]">
+                    Higher
+                  </p>
+                </button>
+
+                <button className="border p-3 rounded-[2rem] text-[#FFF879] hover:bg-[#181A1B] hover:text-white">
+                  <p className="text-extrabold text-xl font-[RooneySansRegular]">
+                    Lower
+                  </p>
+                </button>
+
+                <p className="text-xl font-[RooneySansRegular]">value</p>
+              </div>
+            </div>
+
+            <div className="h-screen w-[150%] text-white bg-[url('https://assets.coingecko.com/coins/images/1/large/bitcoin.png')] brightness-75 bg-no-repeat bg-cover bg-center blur-xl"></div>
+          </CarouselItem>
+
+          <CarouselItem className="relative basis-1/2 bg-white m-0">
+            <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-fit h-fit z-10 bg-red-500">
+              Okazoekazo
+            </div>
+
+            <div className="h-screen w-[150%] text-white bg-[url('https://assets.coingecko.com/coins/images/1/large/bitcoin.png')] brightness-75 bg-no-repeat bg-cover bg-center blur-xl"></div>
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
     </>
   );
-}
+};
 
 export default Game;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true,
-        port: 5173,
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
 });
