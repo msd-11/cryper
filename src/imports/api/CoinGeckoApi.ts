80f5a3b5f9c8336b8d098c4bcf62318bc9c756eb
CastelJeremy <39135270+CastelJeremy@users.noreply.github.com>
2024-02-23 13:11:49 +0100

    feat: get a random coin and shuffle the list simpler code test: add more tests

--- src/imports/api/CoinGeckoApi.ts
+++ src/imports/api/CoinGeckoApi.ts
@@ -2,13 +2,11 @@ import Coin from './Coin';
 
 class CoinGeckoApi {
   private static url: string = 'https://api.coingecko.com/api';
+  private static cache: Coin[] | null = null;
 
-  public static async getCoinsMarkets(
-    currency: string,
-    page: number
-  ): Promise<Coin[]> {
+  public static async getCoinsMarkets(page: number): Promise<Coin[]> {
     const res = await fetch(
-      `${this.url}/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=250&page=${page}&sparkline=false&locale=en`
+      `${this.url}/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=250&page=${page}&sparkline=false&locale=en`
     );
     const json = await res.json();
 
@@ -17,11 +15,28 @@ class CoinGeckoApi {
     );
   }
 
-  public static async getTopCoins(currency: string): Promise<Coin[]> {
-    let topCoins = await this.getCoinsMarkets(currency, 1);
-    topCoins = topCoins.concat(await this.getCoinsMarkets(currency, 2));
+  public static async getTopCoins(): Promise<Coin[]> {
+    if (this.cache == null) {
+      let topCoins = await this.getCoinsMarkets(1);
+      topCoins = topCoins.concat(await this.getCoinsMarkets(2));
 
-    return topCoins;
+      for (let i = topCoins.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [topCoins[i], topCoins[j]] = [topCoins[j], topCoins[i]];
+      }
+
+      this.cache = topCoins;
+    }
+
+    return this.cache;
+  }
+
+  public static async getRandom(): Promise<Coin> {
+    if (this.cache == null) {
+      await this.getTopCoins();
+    }
+
+    return this.cache!.pop()!;
   }
 }
 
