49e5a79b870824cff7ca62ecc866439bb3584c73
msd-11 <msd0xB@pm.me>
2024-02-23 15:48:21 +0100

    Add game logic

--- src/imports/views/Game.tsx
+++ src/imports/views/Game.tsx
@@ -10,86 +10,129 @@ const data = [
   { name: "Bitcoin", value: 1255.0 },
   { name: "Etherum", value: 121 },
   { name: "Nano", value: 255 },
+  { name: "azeaz", value: 255 },
 ];
 
 const Game: React.FC = () => {
   const [api, setApi] = useState<CarouselApi>();
 
   const [current, setCurrent] = useState(0);
-  const [count, setCount] = useState(0);
+  const [lost, setLost] = useState(false);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     if (!api) {
       return;
     }
 
-    setCount(api.scrollSnapList().length);
-    setCurrent(api.selectedScrollSnap() + 1);
+    setCurrent(api.selectedScrollSnap());
 
     api.on("select", () => {
       console.log("log");
-      setCount(count + 1);
     });
   }, [api]);
 
-  const handleClick = () => {
-    console.log("ok");
-    api?.scrollNext();
+  const handleClickHigher = () => {
+    console.log(data[current].value, data[current + 1].value);
+    if (data[current].value <= data[current + 1].value) {
+      setScore(score + 1);
+      api?.scrollNext();
+      setCurrent(current + 1);
+    } else {
+      setLost(true);
+      console.log("PERDU");
+    }
+  };
+
+  const handleClickLower = () => {
+    console.log(data[current].value, data[current + 1].value);
+    if (data[current].value >= data[current + 1].value) {
+      setScore(score + 1);
+      api?.scrollNext();
+      setCurrent(current + 1);
+    } else {
+      setLost(true);
+      console.log("PERDU");
+    }
   };
 
   return (
     <>
+      <p className="absolute bottom-4 left-4 z-20">Score : {score}</p>
+      {lost ? (
+        <div className="fixed top-0 left-0 w-full h-full  z-50 flex justify-center items-center">
+          <div className="absolute top-0 left-0 right-0 bottom-0 w-fit h-fit text-white font-bold z-50 m-auto">
+            <p className="text-4xl">You Lost!</p>
+            <p>Solve this riddle to continue :</p>
+            <p className="mt-4">Riddle...</p>
+            <input
+              className="border bg-white mt-4 p-2 rounded-2xl"
+              placeholder="Response..."
+            ></input>
+            <button className="ml-4 bg-yellow-500 px-4 py-2 rounded-2xl">
+              Guess
+            </button>
+          </div>
+
+          <div className="w-full h-full bg-black opacity-75"></div>
+        </div>
+      ) : (
+        false
+      )}
       <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-28 h-28 bg-[#181A1B] z-10 rounded-[50%] flex justify-center items-center">
         <p className="text-[3vh] font-[RooneySansRegular]">VS</p>
       </div>
-      <Carousel setApi={setApi} opts={{ watchDrag: false, slidesToScroll: 2 }}>
+      <Carousel
+        setApi={setApi}
+        opts={{ watchDrag: false, slidesToScroll: 1, align: "start" }}
+      >
         <CarouselContent>
-          <CarouselItem className="relative basis-1/2 bg-white m-0">
-            <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-fit h-fit z-10 text-center">
-              <p className="text-[6vh] font-[RooneySansRegular]">“Greece”</p>
-              <p className="text-xl font-[RooneySansRegular]">has</p>
-              <p
-                onClick={handleClick}
-                className="text-[6vh] text-[#FFF879] font-[RooneySansRegular]"
-              >
-                673,000
-              </p>
-              <p className="text-xl font-[RooneySansRegular]">value</p>
-            </div>
-            <div className="h-screen w-[150%] text-white bg-[url('https://assets.coingecko.com/coins/images/1/large/bitcoin.png')] brightness-75 bg-no-repeat bg-cover bg-center blur-xl"></div>
-          </CarouselItem>
-
-          <CarouselItem className="relative basis-1/2 bg-white m-0">
-            <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-fit h-fit z-10 text-center">
-              <p className="text-[6vh] font-[RooneySansRegular]">“Greece”</p>
-              <p className="text-xl font-[RooneySansRegular]">has</p>
-              <div className="flex flex-col gap-4 mt-4">
-                <button className="border p-3 rounded-[2rem] text-[#FFF879] hover:bg-[#181A1B] hover:text-white">
-                  <p className="text-extrabold text-xl font-[RooneySansRegular]">
-                    Higher
+          {data.map((value, index) => (
+            <CarouselItem className="relative basis-1/2 bg-white m-0">
+              {current == index ? (
+                <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-fit h-fit z-10 text-center">
+                  <p className="text-[6vh] font-[RooneySansRegular]">
+                    “{value.name}”
                   </p>
-                </button>
-
-                <button className="border p-3 rounded-[2rem] text-[#FFF879] hover:bg-[#181A1B] hover:text-white">
-                  <p className="text-extrabold text-xl font-[RooneySansRegular]">
-                    Lower
+                  <p className="text-xl font-[RooneySansRegular]">has</p>
+                  <p className="text-[6vh] text-[#FFF879] font-[RooneySansRegular]">
+                    {value.value}
                   </p>
-                </button>
+                  <p className="text-xl font-[RooneySansRegular]">value</p>
+                </div>
+              ) : (
+                <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-fit h-fit z-10 text-center">
+                  <p className="text-[6vh] font-[RooneySansRegular]">
+                    “{value.name}”
+                  </p>
+                  <p className="text-xl font-[RooneySansRegular]">has</p>
+                  <div className="flex flex-col gap-4 mt-4">
+                    <button className="border p-3 rounded-[2rem] text-[#FFF879] hover:bg-[#181A1B] hover:text-white">
+                      <p
+                        onClick={handleClickHigher}
+                        className="text-extrabold text-xl font-[RooneySansRegular]"
+                      >
+                        Higher
+                      </p>
+                    </button>
 
-                <p className="text-xl font-[RooneySansRegular]">value</p>
-              </div>
-            </div>
+                    <button className="border p-3 rounded-[2rem] text-[#FFF879] hover:bg-[#181A1B] hover:text-white">
+                      <p
+                        onClick={handleClickLower}
+                        className="text-extrabold text-xl font-[RooneySansRegular]"
+                      >
+                        Lower
+                      </p>
+                    </button>
 
-            <div className="h-screen w-[150%] text-white bg-[url('https://assets.coingecko.com/coins/images/1/large/bitcoin.png')] brightness-75 bg-no-repeat bg-cover bg-center blur-xl"></div>
-          </CarouselItem>
+                    <p className="text-xl font-[RooneySansRegular]">value</p>
+                  </div>
+                </div>
+              )}
 
-          <CarouselItem className="relative basis-1/2 bg-white m-0">
-            <div className="absolute left-0 right-0 top-0 bottom-0 m-auto w-fit h-fit z-10 bg-red-500">
-              Okazoekazo
-            </div>
-
-            <div className="h-screen w-[150%] text-white bg-[url('https://assets.coingecko.com/coins/images/1/large/bitcoin.png')] brightness-75 bg-no-repeat bg-cover bg-center blur-xl"></div>
-          </CarouselItem>
+              <div className="h-screen w-[150%] text-white bg-[url('https://assets.coingecko.com/coins/images/1/large/bitcoin.png')] brightness-75 bg-no-repeat bg-cover bg-center blur-xl"></div>
+            </CarouselItem>
+          ))}
         </CarouselContent>
       </Carousel>
     </>
